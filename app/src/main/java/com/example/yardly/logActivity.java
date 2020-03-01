74e4c582ead652f4685fb10da484252fe319fd4b
kevinpelaez <kevin.pelaez@javeriana.edu.co>
2020-03-01 17:25:03 -0500

    Landscape mejorado

--- app/src/main/java/com/example/yardly/logActivity.java
+++ app/src/main/java/com/example/yardly/logActivity.java
@@ -2,13 +2,24 @@ package com.example.yardly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class logActivity extends AppCompatActivity {
-
+    Button registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        registro = findViewById(R.id.botonRegistrar);
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registro = new Intent(getBaseContext(),Registro.class);
+                startActivity(registro);
+            }
+        });
     }
 }
