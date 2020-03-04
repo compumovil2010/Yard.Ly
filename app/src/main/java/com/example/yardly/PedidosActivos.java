6b7928accc41aaf536e1664fa1324f3b0326f349
CAMILO EDUARDO CRUZ GUTIERREZ <cruzcamilo@javeriana.edu.co>
2020-03-04 01:41:49 -0500

    pedidosSemifin

---
+++ app/src/main/java/com/example/yardly/PedidosActivos.java
@@ -0,0 +1,14 @@
+package com.example.yardly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PedidosActivos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedidos_activos);
+    }
+}
