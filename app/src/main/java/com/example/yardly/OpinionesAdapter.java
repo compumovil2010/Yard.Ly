cee8dec68cf979c845b3949a4351ac58fe591a30
CAMILO ANDRÉS RUIZ URIBE <ruizcamilo@javeriana.edu.co>
2020-03-03 19:09:49 -0500

    Informacion Producto

--- app/src/main/java/com/example/yardly/OpinionesAdapter.java
+++ app/src/main/java/com/example/yardly/OpinionesAdapter.java
@@ -3,83 +3,58 @@ package com.example.yardly;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.DataSetObserver;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import com.example.yardly.R;
 
-public class OpinionesAdapter implements ListAdapter {
-    public View newView(Context context, ViewGroup parent) {
-        return LayoutInflater.from(context)
-                .inflate(R.layout.opiniones, parent, false);
+import java.util.List;
+
+public class OpinionesAdapter extends ArrayAdapter<String> {
+    private int LayoutUso;
+    private Context contex;
+    public OpinionesAdapter (Context context, int resource, List<String> opinions){
+        super(context,resource,opinions);
+        this.LayoutUso=resource;
+        this.contex=context;
     }
-    public void bindView(View view, Context context) {
-        TextView asunto = (TextView) view.findViewById(R.id.asunto);
-        TextView comentario = (TextView) view.findViewById(R.id.comentario);
-        asunto.setText("asunto");
-        comentario.setText("comentario1");
-    }
-
-    @Override
-    public boolean areAllItemsEnabled() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled(int position) {
-        return false;
-    }
-
-    @Override
-    public void registerDataSetObserver(DataSetObserver observer) {
-
-    }
-
-    @Override
-    public void unregisterDataSetObserver(DataSetObserver observer) {
-
-    }
-
-    @Override
-    public int getCount() {
-        return 0;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return false;
-    }
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View vie = convertView;
+        if (vie==null){
+            vie = LayoutInflater.from(getContex())
+                    .inflate(getLayoutUso(), parent, false);
+        }
+        String val = getItem(position);
+        if (val!=null){
+            TextView asunto = vie.findViewById(R.id.asunto);
+            asunto.setText(val);
+        }
+        String val1 = getItem(position);
+        if (val!=null){
+            TextView comentario = vie.findViewById(R.id.comentario);
+            comentario.setText(val1);
+        }
+        return vie;
+    }
+    public int getLayoutUso() {
+        return LayoutUso;
     }
 
-    @Override
-    public int getItemViewType(int position) {
-        return 0;
+    public void setLayoutUso(int layoutUso) {
+        LayoutUso = layoutUso;
     }
 
-    @Override
-    public int getViewTypeCount() {
-        return 0;
+    public Context getContex() {
+        return contex;
     }
 
-    @Override
-    public boolean isEmpty() {
-        return false;
+    public void setContex(Context contex) {
+        this.contex = contex;
     }
 }
--- app/src/main/java/com/example/yardly/Producto.java
+++ app/src/main/java/com/example/yardly/Producto.java
@@ -6,19 +6,26 @@ import android.os.Bundle;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Producto extends AppCompatActivity {
-    String[] mProjection;
+    List<String> mProjection= new ArrayList<>();
     ListView mlista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_producto);
         mlista=findViewById(R.id.opiniones);
-        mProjection = new String[]{
-            "asunto",
-            "comentario"
-        };
-        OpinionesAdapter opAdapter = new OpinionesAdapter();
+        mProjection.add("Asunto");
+        mProjection.add("Opinion");
+        mProjection.add("Asunto");
+        mProjection.add("Opinion");
+        mProjection.add("Asunto");
+        mProjection.add("Opinion");
+        mProjection.add("Asunto");
+        mProjection.add("Opinion");
+        OpinionesAdapter opAdapter = new OpinionesAdapter(this, R.layout.opiniones,mProjection);
         mlista.setAdapter(opAdapter);
     }
 }
