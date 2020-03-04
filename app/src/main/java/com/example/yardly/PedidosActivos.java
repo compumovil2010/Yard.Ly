d4466accf915a56e5aedd48ddf698657f84c5b5d
CAMILO EDUARDO CRUZ GUTIERREZ <cruzcamilo@javeriana.edu.co>
2020-03-04 02:14:15 -0500

    semiSemifinalPEdidosActivos

--- app/src/main/java/com/example/yardly/PedidosActivos.java
+++ app/src/main/java/com/example/yardly/PedidosActivos.java
@@ -3,12 +3,28 @@ package com.example.yardly;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PedidosActivos extends AppCompatActivity {
-
+    List<Producto> mProjection= new ArrayList<>();
+    ListView mlista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedidos_activos);
+        mlista=findViewById(R.id.lisProduc);
+        Producto p = new Producto();
+        p.setNombre("Nombre");
+        p.setDescripcion("Descripcion");
+        p.setPrecio(123456);
+        mProjection.add(p);
+        mProjection.add(p);
+        mProjection.add(p);
+        mProjection.add(p);
+        ProductosAdapter poAdapter = new ProductosAdapter(this, R.layout.pedidos_activos_adapter,mProjection);
+        mlista.setAdapter(poAdapter);
     }
 }
