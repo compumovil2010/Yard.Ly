296068c7d183d51ac33fa6f5d75a683e8a06f2cd
CAMILO EDUARDO CRUZ GUTIERREZ <cruzcamilo@javeriana.edu.co>
2020-03-03 22:45:57 -0500

    info Establecimiento

---
+++ app/src/main/java/com/example/yardly/ProductosAdapter.java
@@ -0,0 +1,4 @@
+package com.example.yardly;
+
+public class ProductosAdapter {
+}
---
+++ app/src/main/java/com/example/yardly/VerInformacionEstablecimiento.java
@@ -0,0 +1,14 @@
+package com.example.yardly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerInformacionEstablecimiento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_informacion_establecimiento);
+    }
+}
