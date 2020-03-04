3b8fbf250684686b61e343e44ab870a75246993f
juanseleon1 <juanseleon1@hotmail.com>
2020-03-03 21:34:56 -0500

    EditarPErfil

--- app/src/main/java/com/example/yardly/editarPerfilU.java
+++ app/src/main/java/com/example/yardly/editarPerfilU.java
@@ -2,13 +2,25 @@ package com.example.yardly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class editarPerfilU extends AppCompatActivity {
 
+    Button direccion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil_u);
+
+        direccion=findViewById(R.id.dirFrec);
+        direccion.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i= new Intent(v.getContext(),dirFrec.class);
+                startActivity(i);
+            }
+        });
     }
 }
