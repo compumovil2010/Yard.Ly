8f1a273ab40e535dd2ef9256a4dd691003453592
kevinpelaez <kevin.pelaez@javeriana.edu.co>
2020-03-04 01:48:50 -0500

    "Creación de la pantalla para ver los pedidos"

---
+++ app/src/main/java/com/example/yardly/OrdenesAdapter.java
@@ -0,0 +1,40 @@
+package com.example.yardly;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class OrdenesAdapter extends ArrayAdapter {
+    private int LayoutUso;
+    private Context context;
+    private List<Orden> ordenes;
+    public OrdenesAdapter(Context context, int resource, List<Orden> ordenes)
+    {
+        super(context,resource,ordenes);
+        this.LayoutUso=resource;
+        this.context=context;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vie = convertView;
+        if (vie==null){
+            vie = LayoutInflater.from(getContext())
+                    .inflate(LayoutUso, parent, false);
+        }
+        Orden pro = (Orden) getItem(position);
+        if (pro!=null){
+            TextView nombre = vie.findViewById(R.id.nombreProducto);
+            TextView fecha = vie.findViewById(R.id.fechaPedido);
+            TextView precio = vie.findViewById(R.id.precio);
+            nombre.setText(pro.getNombreProducto());
+            fecha.setText(pro.getFecha());
+            precio.setText(String.valueOf(pro.getPrecio()));
+        }
+        return  vie;
+    }
+}
