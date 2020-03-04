0a045d5a617bb0363ee15da4f1612da634a6293f
juanseleon1 <juanseleon1@hotmail.com>
2020-03-03 23:52:33 -0500

    Perfil Empresa

--- app/src/main/java/com/example/yardly/dirFrec.java
+++ app/src/main/java/com/example/yardly/dirFrec.java
@@ -9,17 +9,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class dirFrec extends AppCompatActivity {
-    List<String> mProjection= new ArrayList<>();
+    List<LugarEntrega> mProjection= new ArrayList<>();
     ListView mlista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dir_frec);
         mlista=findViewById(R.id.listaDir);
-        mProjection.add("Direccion");
-        mProjection.add("Direccion");
-        mProjection.add("Direccion");
-        mProjection.add("Direccion");
+        LugarEntrega l;
+        for(int i=0; i<5;i++)
+        {
+            l=new LugarEntrega();
+            l.setDireccion("Cra X # Y Z");
+            mProjection.add(l);
+        }
         DireccionesAdapter dAdapter = new DireccionesAdapter(this, R.layout.verdire,mProjection);
         mlista.setAdapter(dAdapter);
     }
