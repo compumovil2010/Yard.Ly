01b144c12309bfacacfb8e776e7b23096f8e5387
CAMILO EDUARDO CRUZ GUTIERREZ <cruzcamilo@javeriana.edu.co>
2020-03-03 22:48:11 -0500

    info Establecimiento

--- app/src/main/java/com/example/yardly/Producto.java
+++ app/src/main/java/com/example/yardly/Producto.java
@@ -12,6 +12,9 @@ import java.util.List;
 public class Producto extends AppCompatActivity {
     List<String> mProjection= new ArrayList<>();
     ListView mlista;
+    private String nombre;
+    private int precio;
+    private String Descripcion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,4 +31,28 @@ public class Producto extends AppCompatActivity {
         OpinionesAdapter opAdapter = new OpinionesAdapter(this, R.layout.opiniones,mProjection);
         mlista.setAdapter(opAdapter);
     }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
 }
