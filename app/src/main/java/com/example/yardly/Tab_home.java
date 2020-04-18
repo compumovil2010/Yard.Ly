44846b678408ae88d3dc534524b78db8297c3adc
AMendoza04 <daniel.mendo04@gmail.com>
2020-04-18 15:24:51 -0500

    btn_comida en home

--- app/src/main/java/com/example/yardly/Tab_home.java
+++ app/src/main/java/com/example/yardly/Tab_home.java
@@ -1,9 +1,11 @@
 package com.example.yardly;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,14 +13,21 @@ import androidx.fragment.app.Fragment;
 
 public class Tab_home extends Fragment {
 
+    Button btn_comida;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.tab_home, container, false);
+        btn_comida = v.findViewById( R.id.btn_comida );
 
+        btn_comida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent registro = new Intent( v.getContext(),omgreen.class );
+                startActivity( registro );
 
-
-
+            }
+        });
 
 
         return v;
