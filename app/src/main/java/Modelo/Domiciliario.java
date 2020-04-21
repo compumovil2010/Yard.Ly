f146aa2862ae6b2a927aafc3b16b547ed6f709c8
juanseleon1 <juanseleon1@hotmail.com>
2020-04-21 18:05:11 -0500

    Modelos Basicos

--- app/src/main/java/Modelo/Domiciliario.java
+++ app/src/main/java/Modelo/Domiciliario.java
@@ -1,4 +1,35 @@
 package Modelo;
 
 public class Domiciliario extends Usuario {
+    String pedidoActual;
+    public  static  final String PATH_USERS = "domiciliario/";
+    public static final String PATH_PROFILE_PHOTO = "domiciliario/profile_photos";
+    public double lat,longi;
+
+    public Domiciliario() {
+    }
+
+    public String getPedidoActual() {
+        return pedidoActual;
+    }
+
+    public void setPedidoActual(String pedidoActual) {
+        this.pedidoActual = pedidoActual;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLongi() {
+        return longi;
+    }
+
+    public void setLongi(double longi) {
+        this.longi = longi;
+    }
 }
